9762ea6647fb980e4c0c83871eb5f0a6ac129197
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-06 21:55:19 +0530

    Components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='bg-purple-600 p-4 text-white'>React Router Dom</h1>
+    
     </>
   )
 }
